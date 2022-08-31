62d3d7856a19ab72db33638e2b60b54b2cd5c6bf
s-sahil1 <35522593+s-sahil1@users.noreply.github.com>
2022-08-31 14:22:53 -0300

    update1.0.2

--- index.js
+++ index.js
@@ -16,12 +16,12 @@ $("input").click(function() {
       $(this).attr("value", player1);
       turn = 1;
       checkTie();
-        checkWinnder();
+      checkWinnder();
     } else if (turn === 1) {
       $(this).attr("value", player2);
       turn = 0;
       checkTie();
-        checkWinnder();
+      checkWinnder();
     }
   }
   console.log($(this).attr("value"));
@@ -45,12 +45,16 @@ function checkWinnder() {
     if ($("#1").attr("value") === "X") {
       console.log("X  Wins!!");
       $("h1").text("X  Wins!!");
+      blink("#1", "#2", "#3");
       winner = "y";
     } else if ($("#1").attr("value") === "O") {
       console.log("O  Wins!!");
       $("h1").text("O  Wins!!");
       winner = "y";
+      blink("#1", "#2", "#3");
     }
+
+
   }
 
   //Second Row winning
@@ -59,11 +63,14 @@ function checkWinnder() {
       console.log("X  Wins!!");
       $("h1").text("X  Wins!!");
       winner = "y";
+      blink("#4", "#5", "#6");
     } else if ($("#4").attr("value") === "O") {
       console.log("O  Wins!!");
       $("h1").text("O  Wins!!");
       winner = "y";
+      blink("#4", "#5", "#6");
     }
+
   }
 
   //Third Row winning
@@ -71,12 +78,15 @@ function checkWinnder() {
     if ($("#7").attr("value") === "X") {
       console.log("X  Wins!!");
       $("h1").text("X  Wins!!");
+      blink("#7", "#8", "#9");
       winner = "y";
     } else if ($("#7").attr("value") === "O") {
       console.log("O  Wins!!");
       $("h1").text("O  Wins!!");
       winner = "y";
+      blink("#7", "#8", "#9");
     }
+
   }
 
   //First Column winning
@@ -85,11 +95,14 @@ function checkWinnder() {
       console.log("X  Wins!!");
       $("h1").text("X  Wins!!");
       winner = "y";
+      blink("#1", "#4", "#7");
     } else if ($("#1").attr("value") === "O") {
       console.log("O  Wins!!");
       $("h1").text("O  Wins!!");
       winner = "y";
+      blink("#1", "#4", "#7");
     }
+
   }
 
   //Second Column winning
@@ -98,11 +111,13 @@ function checkWinnder() {
       console.log("X  Wins!!");
       $("h1").text("X  Wins!!");
       winner = "y";
+      blink("#2", "#5", "#8");
     } else if ($("#2").attr("value") === "O") {
       console.log("O  Wins!!");
       $("h1").text("O  Wins!!");
-      winner = "y";
+      blink("#2", "#5", "#8");
     }
+
   }
 
   //Third Column winning
@@ -111,11 +126,14 @@ function checkWinnder() {
       console.log("X  Wins!!");
       $("h1").text("X  Wins!!");
       winner = "y";
+      blink("#3", "#6", "#9");
     } else if ($("#3").attr("value") === "O") {
       console.log("O  Wins!!");
       $("h1").text("O  Wins!!");
       winner = "y";
+      blink("#3", "#6", "#9");
     }
+
   }
 
   //First Diagonal winning
@@ -124,10 +142,12 @@ function checkWinnder() {
       console.log("X  Wins!!");
       $("h1").text("X  Wins!!");
       winner = "y";
+      blink("#1", "#5", "#9");
     } else if ($("#1").attr("value") === "O") {
       console.log("O  Wins!!");
       $("h1").text("O  Wins!!");
       winner = "y";
+      blink("#1", "#5", "#9");
     }
   }
 
@@ -137,10 +157,12 @@ function checkWinnder() {
       console.log("X  Wins!!");
       $("h1").text("X  Wins!!");
       winner = "y";
+      blink("#3", "#5", "#7");
     } else if ($("#3").attr("value") === "O") {
       console.log("O  Wins!!");
       $("h1").text("O  Wins!!");
       winner = "y";
+      blink("#3", "#5", "#7");
     }
   }
 
@@ -160,4 +182,27 @@ function checkTie() {
   if (numOfClicks === 9) {
     $("h1").text("Its A Tie");
   }
+}
+
+function blink(tile1, tile2, tile3) {
+
+  var i = 1;
+
+  var x = setInterval(function() {
+    $(tile1).toggleClass("clicked");
+    $(tile2).toggleClass("clicked");
+    $(tile3).toggleClass("clicked");
+    console.log(i);
+
+    if(i>=8){
+      clearInterval(x);
+    }
+    i++;
+  }, 200);
+
+  setTimeout(function(){
+    alert("Please reset the game!")
+  }, 1800);
+
+
 }
